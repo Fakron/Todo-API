f14dceaba525299234c006ea6b879a4a6bdb64c7
Fakron <rushalchitrakar4@gmail.com>
2022-10-22 17:58:11 +0545

    Added requirements.txt and CORS header

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -42,16 +42,28 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'rest_framework_swagger',
+    "corsheaders",
+
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000",
+    "https://fulltodo.netlify.app",
 ]
 
 ROOT_URLCONF = "todo_app.urls"
